d5e399a8f8ecf3d40a1833ca5e65aaea74825eaa
Alan Cutter <alancutter@chromium.org>
2014-06-06 14:51:33 +1000

    Ensure perf_week page set is up to date when running big benchmarks

--- PerformanceTests/Animation/PerfWeek/run-big-benchmarks.py
+++ PerformanceTests/Animation/PerfWeek/run-big-benchmarks.py
@@ -14,14 +14,17 @@ src_path = current_path[:current_path.index('/src') + 4]
 perf_tools_path = src_path + '/tools/perf'
 page_sets_path = perf_tools_path + '/page_sets'
 perf_week_path = src_path + '/third_party/WebKit/PerformanceTests/Animation/PerfWeek'
+resource_perf_week_page_set_path = perf_week_path + '/resources/perf_week.py'
+smoothness_perf_week_page_set_path = page_sets_path + '/perf_week.py'
 
 
 def ensureSmoothnessPerfWeekBenchmark():
-  if not os.path.exists(page_sets_path + '/perf_week.py'):
-    subprocess.call(['cp', perf_week_path + '/resources/perf_week.py', page_sets_path])
+  if not os.path.exists(smoothness_perf_week_page_set_path):
     with open(perf_tools_path + '/benchmarks/smoothness.py', 'a') as smoothness_benchmarks:
       smoothness_benchmarks.write(open(perf_week_path + '/resources/smoothness_perf_week.py').read())
-    print('Patched existing smoothness benchmarks with perf_week BigBenchmarks target')
+      print('Patched existing smoothness benchmarks with perf_week BigBenchmarks target')
+  # Ensure the version of the perf_week.py page set is up to date.
+  subprocess.call(['cp', resource_perf_week_page_set_path, smoothness_perf_week_page_set_path])
 
 def refreshPerfWeekFolder():
   source = perf_week_path + '/BigBenchmarks'
