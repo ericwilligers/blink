ace052e9cccfa65c0c5df26f86ffbce30e53c2cd
Alan Cutter <alancutter@chromium.org>
2014-06-06 14:34:51 +1000

    Move measurementReady into the window object to avoid having to call checkWebAnimationsAPI()

--- PerformanceTests/Animation/PerfWeek/BigBenchmarks/resources/web-animations-api-check.js
+++ PerformanceTests/Animation/PerfWeek/BigBenchmarks/resources/web-animations-api-check.js
@@ -1,7 +1,5 @@
-function checkWebAnimationsAPI() {
-  if (!Element.prototype.animate) {
-    // Generate 0 frames and fail if Web Animations is not present.
-    document.documentElement.style.display = 'none';
-    window.measurementReady = true;
-  }
+if (!Element.prototype.animate) {
+  // Generate 0 frames and fail if Web Animations is not present.
+  document.documentElement.style.display = 'none';
+  window.measurementReady = true;
 }
